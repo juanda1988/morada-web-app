e1346fe7c2d34550bc78ece92b7d42958f08f7f3
juanda1988 <jdmarti12@gmail.com>
2022-04-19 16:49:46 -0500

    Correcciones al proyecto

--- src/pages/Home/components/Hello/index.js
+++ src/pages/Home/components/Hello/index.js
@@ -1,10 +1,11 @@
 import { Fragment } from "react";
 import { PropertyTypesContainer } from "../../styles";
+import myimage from "../Hello/th.jpg"
 
 export const Hello = ({name}) => (
     <PropertyTypesContainer>
         <Fragment>
-            <img src="C:\Users\Juan David\Documents\Fullsatack Cedesistemas\proyectos\morada-app\morada-web-app\th.jpg" alt="bienvenida"/>
+            <img src={myimage} alt="bienvenida"/>
             <h1>Hi {name}</h1>
         </Fragment>
     </PropertyTypesContainer>
